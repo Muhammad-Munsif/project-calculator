38d99f56c78ae98c895a91f6a4f9f2c73b56fd0b
Muhammad-Munsif <m.munsif4375@gmail.com>
2025-09-13 19:43:11 +0500

    small tasked added to JS file

--- script.js
+++ script.js
@@ -3,17 +3,16 @@ const inputFeild = document.getElementById("input-feild");
 
 // this function will display the value of button
 function showValue(input) {
-    inputFeild.value = inputFeild.value + input; // this is longway to write this code
-    // inputFeild.value += input; // this is the shortend way to rate the code
+  inputFeild.value = inputFeild.value + input; // this is longway to write this code
+  // inputFeild.value += input; // this is the shortend way to rate the code
 }
 
-// This function will clear the input feild display 
-function clearValue(){
-    inputFeild.value = "";
+// This function will clear the input feild display
+function clearValue() {
+  inputFeild.value = "";
 }
 
-// This function will calculates the value which we select the button it will goes through input feild 
-function calcuateValue(){
-    inputFeild.value = eval(inputFeild.value);
+// This function will calculates the value which we select the button it will goes through input feild
+function calcuateValue() {
+  inputFeild.value = eval(inputFeild.value);
 }
-
